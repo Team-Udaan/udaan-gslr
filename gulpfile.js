2a52104cce93dd4ca3a24048de8ca3a8da7e2fc4
Dhruvin Gandhi <dhruvin2910@gmail.com>
2016-03-28 21:14:44 +0530

    Add meta:vp and update gulpfile with concat uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var paths = {
   index: 'src/index.html',
-  templates: 'src/templates/**/*.*',
-  scripts: 'src/scripts/**/*.*',
-  styles: 'src/styles/**/*.*',
+  templates: 'src/templates/**/*.html',
+  scripts: 'src/scripts/**/*.js',
+  styles: 'src/styles/**/*.css',
   dist: 'dist'
 };
 
@@ -23,6 +25,10 @@ gulp.task('copy:templates', function () {
 // Copy Scripts
 gulp.task('copy:scripts', function () {
   return gulp.src(paths.scripts)
+    .pipe(concat('scripts.js'))
+    .pipe(uglify({
+      mangle: false
+    }))
     .pipe(gulp.dest(paths.dist));
 });
 
@@ -34,6 +40,14 @@ gulp.task('copy:styles', function () {
 
 // Build
 gulp.task('build', ['copy:index', 'copy:templates', 'copy:scripts', 'copy:styles']);
+
+// Watch
+gulp.task('watch', function () {
+  gulp.watch(paths.index, ['copy:index']);
+  gulp.watch(paths.templates, ['copy:templates']);
+  gulp.watch(paths.scripts, ['copy:scripts']);
+  gulp.watch(paths.styles, ['copy:styles']);
+});
 
 // Default
 gulp.task('default', ['build']);
