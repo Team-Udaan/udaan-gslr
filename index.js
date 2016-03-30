64ffb19c57cf002ff3bbe073e3dfe6afb291e2b3
Dhruvin Gandhi <dhruvin2910@gmail.com>
2016-03-31 03:55:44 +0530

    Create websocket echo server

---
+++ index.js
@@ -0,0 +1,26 @@
+var WebSocket = require('ws');
+var WebSocketServer = WebSocket.Server;
+
+var port = process.env.port;
+
+if (!port) {
+  console.error('Port is not defined as environmental variable.');
+  return 1;
+}
+
+var wss = new WebSocketServer({
+  port: port
+});
+
+wss.on('open', function () {
+  console.log('WS is listening: http://localhost:' + port);
+});
+
+wss.on('connection', function (socket) {
+
+  socket.on('message', function (data) {
+    console.log(data);
+    socket.send(data);
+  });
+
+});
