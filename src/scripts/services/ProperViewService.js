01b2d4abe3154a1eef01c6fa01ae856d6ca8f8cc
Dhruvin Gandhi <dhruvin2910@gmail.com>
2016-03-31 22:28:01 +0530

    Update ProperViewService to handle registered and loggedIn states

--- src/scripts/services/ProperViewService.js
+++ src/scripts/services/ProperViewService.js
@@ -5,17 +5,31 @@
     .service('ProperViewService', [
       'WebSocketService',
       '$location',
-      function (WebSocketService, $location) {
+      'State',
+      function (WebSocketService, $location, State) {
         return function () {
           var view = $location.url();
           var connected = WebSocketService.socket.status.connected;
+
+          // Is on `to connect` view and connected already
           if (view == '/connect' && connected) {
             $location.url('/register');
             return false;
-          } else if (view != '/connect' && !connected) {
-            $location.url('/connect');
+          }
+
+          // Is on `to register` view and registered already
+          if (view == '/register' && State.user.registered) {
+            $location.url('/login');
             return false;
-          } else return true;
+          }
+
+          // Is on `to login` view and loggedIn already
+          if (view != '/login' && State.user.loggedIn) {
+            $location.url('/poll');
+            return false;
+          }
+
+          return true;
         }
       }]);
 
