1dd5f8e37e36a367ca1e5db07e8ab7f98405a4d5
Dhruvin Gandhi <dhruvin2910@gmail.com>
2016-03-31 22:18:47 +0530

    Require metadata from server to bootstrapp app

--- src/scripts/controllers/GSLRCtrl.js
+++ src/scripts/controllers/GSLRCtrl.js
@@ -6,11 +6,27 @@
       'WebSocketService',
       'ProperViewService',
       '$scope',
-      function (WebSocketService, ProperViewService, $scope) {
+      'ChannelService',
+      'State',
+      function (WebSocketService, ProperViewService, $scope, ChannelService, State) {
 
         WebSocketService.socket.hooks.open = WebSocketService.socket.hooks.close = function () {
           $scope.$apply(ProperViewService);
         };
+
+        WebSocketService.socket.hooks.bootstrap = function () {
+          ChannelService.meta.emit();
+        };
+
+        ChannelService.meta.listenOnce(function (data) {
+          State.name = data.name;
+          State.year = data.year;
+          State.gs.name = data.gs.name;
+          State.gs.mascot = data.gs.mascot;
+          State.lr.name = data.lr.name;
+          State.lr.mascot = data.lr.mascot;
+          document.title = State.name + ' ' + State.year;
+        });
 
       }]);
 
